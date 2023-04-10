84e9b35df48118f5cea2836d2a12a228e74dc588
bohdan <username.qwerty222@gmail.com>
2023-04-10 15:07:25 +0200

    Refactor ec2 terraform to use with jenkins and consul

--- dns-deploy/ec2/ec2.tf
+++ dns-deploy/ec2/ec2.tf
@@ -1,18 +1,55 @@
 module "ec2" {
-    source = "../modules/ec2"
+    source = "../../modules/ec2"
 
     instances           = ["dns", "webserver", "user"]
-    instance_type       = "t2.micro"
-    ami                 = "ami-03e08697c325f02ab"
+    instance_type       = data.consul_keys.ec2.var.instance_type
+    ami                 = data.consul_keys.ec2.var.ami
 
-    availability_zone   = var.availability_zone
-    subnet_id           = module.network.subnet_id
+    availability_zone   = var.AWS_AVAILABILITY_ZONE
+    subnet_id           = data.consul_keys.network.var.subnet_id
 
-    key_name            = module.common_key.name
-    sec_group_ids       = module.security_groups_common.ids
+    key_name            = data.consul_keys.ssh_key.var.id
+    sec_group_ids       = jsondecode(data.consul_keys.security_groups.var.ids)
 }
 
+data "consul_keys" "ec2" {
+    key { 
+        name = "ami" 
+        path = "${var.PROJECT_NAME}/ec2/ami"
+    }
 
+    key {
+        name = "instance_type" 
+        path = "${var.PROJECT_NAME}/ec2/instance_type"
+    }
+}
 
+data "consul_keys" "network" {
+    key {
+        name = "subnet_id"
+        path = "${var.PROJECT_NAME}/network/subnet/id"
+    }
+}
 
+data "consul_keys" "ssh_key" {
+    key {
+        name = "id"
+        path = "${var.PROJECT_NAME}/ssh_keys/common/id"
+    }
+}
 
+data "consul_keys" "security_groups" {
+    key {
+        name = "ids"
+        path = "${var.PROJECT_NAME}/security_groups/ids"
+    }
+}
+
+module "consul_push" {
+    source = "../../modules/consul_kv"
+
+    push_lists = [
+        { path = "dns-deploy/ec2/ids", value = module.ec2.ids_json},
+        { path = "dns-deploy/ec2/ids", value = module.ec2.ips_json},
+    ]
+}
