1ecf082462fd974f86a30bc5e215c388c957eccd
name <username.qwerty222@gmail.com>
2023-04-11 09:57:44 +0200

    Add comments to variables

--- dns-deploy/ssh_keys/variables.tf
+++ dns-deploy/ssh_keys/variables.tf
@@ -21,20 +21,24 @@ variable "AWS_REGION" {
 variable "PROJECT_NAME" {
     type        = string
     default     = null
+    description = "Name of the project, address to consul KV depends on it"
 }
 
 variable "CONSUL_IP" {
     type        = string
     default     = null
+    description = "IP address of consul server, where consul KV is running"
 }
 
 variable "CONSUL_DC" {
     type        = string
     default     = null
+    description = "Consul server datacenter"
 }
 
 variable "CONSUL_TOKEN" {
     type        = string
     default     = null
     sensitive   = true
+    description = "Access token"
 }
