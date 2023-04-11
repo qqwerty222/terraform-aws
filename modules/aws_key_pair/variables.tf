0543a14e0a814a893f2946a456fe5598e553eb59
name <username.qwerty222@gmail.com>
2023-04-11 10:26:13 +0200

    Add comments to variables

--- modules/aws_key_pair/variables.tf
+++ modules/aws_key_pair/variables.tf
@@ -2,6 +2,7 @@ variable "key" {
     type        = map(string)
     default     = null
     sensitive   = true
+    description = "Map that contain key_name and pub_key. { key_name = orange, pub_key = ssh-rsa12312321 }"
 }
 
 # variable "key_list" {
