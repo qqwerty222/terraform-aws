de069eb2fb9bc7669e5790da08017033923e5eab
name <username.qwerty222@gmail.com>
2023-04-11 10:23:55 +0200

    Add comments

--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -7,39 +7,42 @@ variable "number" {
 variable "ami" {
     type        = string
     default     = null
+    description = "Amazon machine image to use"
 }
 
 variable "instance_type" {
     type        = string
     default     = null
-    description = "type of ec2 instance, t2.micro at example"
+    description = "Type of ec2 instance, t2.micro at example"
 }
 
 variable "availability_zone" {
     type        = string 
     default     = null
+    description = "AWS availability zone to locate instance in"
 }
 
 variable "subnet_id" {
     type        = string
     default     = null
+    description = "ID of subnet where instance will be located"
 }
 
 variable "key_name" {
     type        = string
     default     = null
-    description = "name of ssh key to assign"
+    description = "Name of ssh key assign to instance"
 }
 
 variable "sec_group_ids" {
     type        = list
     default     = null
-    description = "list of sec groups assigned to ec2"
+    description = "List of sec groups assign to instance"
 }
 
 variable "user_data" {
     type        = string
     default     = null
-    description = "cloud init script to while initialization"
+    description = "Cloud-init script to run while initialization"
 }
 
