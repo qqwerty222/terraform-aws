4d7b4b6cc9a04d07ffd0d33036d1821cc72fdf1c
name <username.qwerty222@gmail.com>
2023-04-25 16:32:56 +0200

    Add spaces

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -1,11 +1,19 @@
 resource "aws_vpc" "vpc" {
     cidr_block          = var.vpc_cidr
+
+    tags = {
+        Name = var.vpc_name
+    }
 }
 
 resource "aws_subnet" "subnet" {
     vpc_id            = aws_vpc.vpc.id
     cidr_block        = var.subnet_cidr
     availability_zone = var.availability_zone
+    
+    tags = {
+        Name = var.subnet_name
+    }
 }
 
 
